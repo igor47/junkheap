a08e3f29b34d0adabc8697ea0a6325c08ed8082e
Igor Serebryany <igor47@moomers.org>
2009-12-30 16:17:24 +0000

    better findwords

--- findwords.py
+++ findwords.py
@@ -4,16 +4,20 @@ import sys, os, os.path, re
 
 class wordFinder(object):
 	"""Creates a hash of all words in all files in <path> that start with <letter>"""
-	def __init__(self, path, letter = 'a'):
+	def __init__(self, path, letter = False):
 		"""Creates the variables and starts the search rolling"""
-		self.wordRe = re.compile(r'\b%c\w*\b' % letter, re.IGNORECASE)
+		if letter:
+			self.wordRe = re.compile(r'\b%c\w*\b' % letter, re.IGNORECASE)
+		else:
+			self.wordRe = re.compile(r'\b\w+\b', re.IGNORECASE)
+
 		self.words = {}
 		for dirname, dirs, files in os.walk(path):
 			for f in files:
 				self.findWords(os.path.join(dirname,f))
 
 	def findWords(self, f):
-		"""Puts all the words in file f matching self.wordRe into self.words"""
+		"""Puts all the words in file f that match self.wordRe into self.words"""
 		for line in open(f).readlines():
 			for wordM in self.wordRe.finditer(line):
 				word = wordM.group().lower()
@@ -23,14 +27,18 @@ class wordFinder(object):
 					self.words[word] = 1
 
 if __name__ == "__main__":
-	try:
-		finder = wordFinder(sys.argv[1], sys.argv[2])
-	except IndexError:
-		print "usage: %s <path> <letter>" % sys.argv[0]
+	if len(sys.argv) == 1:
+		print "usage: %s <path> [<letter>]" % sys.argv[0]
+		sys.exit(1)
+	elif len(sys.argv) == 2:
+		finder = wordFinder(sys.argv[1])
 	else:
-		#strange little sorting algorithm - i think this has a name? but I can't remember it
-		toprint = ["%09d|%s" % (count, word) for word, count in finder.words.items()]
-		toprint.sort()												#use python to sort the list in-place
-		for v in toprint:
-			count, word = v.split('|')						#extract the original values
-			print "%s\t:\t%d" % (word, int(count))
+		finder = wordFinder(sys.argv[1], sys.argv[2])
+
+	#strange little sorting algorithm - i think this 
+	#has a name? but I can't remember it
+	toprint = ["%09d|%s" % (count, word) for word, count in finder.words.items()]
+	toprint.sort()					#use python to sort the list in-place
+	for v in toprint:
+		count, word = v.split('|')			#extract the original values
+		print "%s\t:\t%d" % (word, int(count))
